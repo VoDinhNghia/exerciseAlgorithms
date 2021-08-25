134515c5d9f3942b3cecf7a7fd946fc8ee658b5d
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2021-08-25 10:22:43 +0700

    json
    json

---
+++ exerciseJson.js
@@ -0,0 +1,79 @@
+// sap xep json theo ID
+let dataJsonTableUser = [{
+    'ID': 1,
+    'Name': 'Vo A',
+    'Age': 25
+}, {
+    'ID': 9,
+    'Name': 'Nguyen Van B',
+    'Age': 26
+}, {
+    'ID': 3,
+    'Name': 'Nguyen Van A',
+    'Age': 24
+}, {
+    'ID': 2,
+    'Name': 'Tran C',
+    'Age': 25
+}, {
+    'ID': 7,
+    'Name': 'Van Mo',
+    'Age': 28
+}];
+
+let dataJsonTableUserB = [{
+    'ID': 1,
+    'Name': 'Vo A',
+    'Age': 25
+}, {
+    'ID': 9,
+    'Name': 'Nguyen Van B',
+    'Age': 26
+}, {
+    'ID': 12,
+    'Name': 'Van Ba',
+    'Age': 29
+}];
+//merge 2 array lai va sap xep
+let dataMerge2Array = dataJsonTableUser.concat(dataJsonTableUserB);
+console.log(`Data sau khi merge 2 array: ${JSON.stringify(dataMerge2Array)}`);
+//Sap xep array vua merge theo ID
+let dataJsonSortID = dataMerge2Array.sort(function(Id1, Id2) {
+    return Id1.ID - Id2.ID;
+});
+console.log(`Data sau khi duoc sap xep theo ID: ${JSON.stringify(dataJsonSortID)}`);
+//Loai bo nhung ID trung trong data vua sap xep
+let removeDuplicationInData = [dataJsonSortID[0]];
+for (let i = 1; i < dataJsonSortID.length; i++) {
+    if (dataJsonSortID[i - 1].ID != dataJsonSortID[i].ID) {
+        removeDuplicationInData.push(dataJsonSortID[i])
+    }
+}
+console.log(`Data sau khi loai bo trung lap: ${JSON.stringify(removeDuplicationInData)}`);
+//Sap xep theo ten
+let dataJsonSortName = dataMerge2Array.sort(function(Name1, Name2) {
+    let nameA = Name1.Name.toUpperCase();
+    let nameB = Name2.Name.toUpperCase();
+    let comparison = 0;
+    if (nameA > nameB) {
+        comparison = 1;
+    } else if (nameA < nameB) {
+        comparison = -1;
+    }
+    return comparison;
+})
+console.log(`Data sau khi duoc sap xep theo Name: ${JSON.stringify(dataJsonSortName)}`);
+// Su dung forEach de lay thong tin user Vo A tu array data
+let infoUserVoA = {};
+dataMerge2Array.forEach(function(Item) {
+    if (Item.Name == 'Vo A') {
+        infoUserVoA = Item;
+    }
+});
+console.log(`Thong tin user Vo A: ${JSON.stringify(infoUserVoA)}`);
+// replace key Name = userName
+dataMerge2Array.forEach(function(Item) {
+    Item['userName'] = Item['Name'];
+    delete Item['Name'];
+});
+console.log(`Data sau khi replace key Name la: ${JSON.stringify(dataMerge2Array)}`);
